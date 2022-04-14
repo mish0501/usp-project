80efde969401480544b14dddf15577a3e92ea768
ВЛАДИМИР ВЛАДИМИРОВ <vladki4a6@gmail.com>
2022-04-14 15:13:14 +0300

    [ch31] add phone validation and test

--- src/phoneValidation.js
+++ src/phoneValidation.js
@@ -27,12 +27,10 @@ const firstThreeNumsArr = [
 const firstFourNumsArr = ["0601", "0618", "0361", "0301"];
 const regex = /\d{9}$/;
 const regexSofia = /02\d{7}$/;
-const firstThreeNums = homePhone.substring(0, 3);
-const firstFourNums = homePhone.substring(0, 4);
+const mobilePhone = /[0]{1}[8-9]{1}[7-9]{1}\d{7}$/;
 
 export function isPhoneValid(phone) {
-  const phoneMaxNum = /[0]{1}[8-9]{1}[7-9]{1}\d{7}$/;
-  return phoneMaxNum.test(phone);
+  return mobilePhone.test(phone);
 }
 
 export function isHomePhoneValid(homePhone) {
@@ -42,6 +40,7 @@ export function isSofiaPhoneValid(homePhone) {
   return regexSofia.test(homePhone);
 }
 export function isFirstThreeNumsValid(homePhone) {
+  const firstThreeNums = homePhone.substring(0, 3);
   for (let i = 0; i < firstThreeNumsArr.length; i++) {
     if (firstThreeNums === firstThreeNumsArr[i]) {
       return regex.test(homePhone);
@@ -49,6 +48,7 @@ export function isFirstThreeNumsValid(homePhone) {
   }
 }
 export function isFirstFourNumsValid(homePhone) {
+  const firstFourNums = homePhone.substring(0, 4);
   for (let i = 0; i < firstFourNumsArr.length; i++) {
     if (firstFourNums === firstFourNumsArr[i]) {
       return regex.test(homePhone);
