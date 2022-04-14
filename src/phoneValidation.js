70941d448b605968b4ef5ef4a385c5e7dae10571
ВЛАДИМИР ВЛАДИМИРОВ <vladki4a6@gmail.com>
2022-04-14 14:16:31 +0300

    [ch31] add phone validation and test

--- src/phoneValidation.js
+++ src/phoneValidation.js
@@ -1,54 +1,60 @@
+const firstThreeNumsArr = [
+  "054",
+  "092",
+  "064",
+  "052",
+  "068",
+  "058",
+  "082",
+  "066",
+  "062",
+  "084",
+  "086",
+  "094",
+  "096",
+  "056",
+  "044",
+  "042",
+  "046",
+  "038",
+  "034",
+  "032",
+  "076",
+  "073",
+  "078",
+];
+
+const firstFourNumsArr = ["0601", "0618", "0361", "0301"];
+
 export function isPhoneValid(phone) {
   const phoneMaxNum = /[0]{1}[8-9]{1}[7-9]{1}\d{7}$/;
   return phoneMaxNum.test(phone);
 }
 
 export function isHomePhoneValid(homePhone) {
-   var arrPhone = [
-    "054",
-    "092",
-    "064",
-    "052",
-    "068",
-    "058",
-    "082",
-    "066",
-    "062",
-    "084",
-    "086",
-    "094",
-    "096",
-    "056",
-    "044",
-    "042",
-    "046",
-    "038",
-    "034",
-    "032",
-    "076",
-    "073",
-    "078",
-  ];
-  var arrPhone2 = [ "0601", "0618", "0361", "0301"];
-  const regex1 = /[0]{1}[2]{1}\d{7}$/;
-  if(regex1.test(homePhone)){
-    return true;
-  }
-  
-  for(let i = 0; i < arrPhone.length;i++){
-    var firstThreeNums = homePhone.substring(0,3);
-    if(firstThreeNums === arrPhone[i]){
-      let regex = /\d{9}$/
+  isFirstFourNumsValid,
+  isFirstThreeNumsValid,
+  isSofiaPhoneValid
+}
+export function isSofiaPhoneValid(homePhone) {
+  const regex1 = /02\d{7}$/;
+  return regex1.test(homePhone);
+}
+export function isFirstThreeNumsValid(homePhone) {
+  for (let i = 0; i < firstThreeNumsArr.length; i++) {
+    var firstThreeNums = homePhone.substring(0, 3);
+    if (firstThreeNums === firstThreeNumsArr[i]) {
+      let regex = /\d{9}$/;
       return regex.test(homePhone);
     }
   }
-
-  for(let i = 0; i < arrPhone2.length;i++){
-    var firstFourNums = homePhone.substring(0,4);
-    if(firstFourNums === arrPhone2[i]){
-      let regex = /\d{9}$/
+}
+export function isFirstFourNumsValid(homePhone) {
+  for (let i = 0; i < firstFourNumsArr.length; i++) {
+    var firstFourNums = homePhone.substring(0, 4);
+    if (firstFourNums === firstFourNumsArr[i]) {
+      let regex = /\d{9}$/;
       return regex.test(homePhone);
     }
   }
-  return false;
 }
