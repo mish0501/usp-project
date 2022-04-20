fcbca3da0dd62d5d70d81d27ba716af0756dc7d8
posob <rumen_pavlov35@abv.bg>
2022-04-20 17:47:51 +0300

    a

--- src/egnChecker.js
+++ src/egnChecker.js
@@ -1,47 +1,55 @@
+const monthDayArr = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+};
+
 export function isEgnValidGeneral(egn) {
     if (egn.length !== 10) {
         return false;
     }
     
-    var monthDayArr = {
-        1: 31,
-        2: 28,
-        3: 31,
-        4: 30,
-        5: 31,
-        6: 30,
-        7: 31,
-        8: 31,
-        9: 30,
-        10: 31,
-        11: 30,
-        12: 31
-    };
-    var yearRegex = /^[0-9]{2}$/;
+    const yearRegex = /^[0-9]{2}$/;
     if (!yearRegex.test(egn.substring(0, 2))) {
         return false;
     }
 
-    var monthRegex = /^[0-5]{1}/;
-    var segment;
+    const monthRegex = /^[0-5]{1}/;
+    let substract;
+    let segment;
     switch (egn[2]) {
         case '0':
             segment = /[1-9]{1}$/;
+            substract = 0;
             break;
         case '1':
             segment = /[0-2]{1}$/;
+            substract = 0;
             break;
         case '2':
             segment = /[1-9]{1}$/;
+            substract = 20;
             break;
         case '3':
             segment = /[0-2]{1}$/;
+            substract = 20;
             break;
         case '4':
             segment = /[1-9]{1}$/;
+            substract = 40;
             break;
         case '5':
             segment = /[0-2]{1}$/;
+            substract = 40;
             break;
         default:
             return false;
@@ -51,19 +59,17 @@ export function isEgnValidGeneral(egn) {
         return false;
     }
        
-    var month = parseInt(egn.substring(2, 4));
+    var month = parseInt(egn.substring(2, 4)) - substract;
     var day;
-    if(month < 13 || month > 0) {
+    if(month < 13 && month > 0) {
         day = monthDayArr[month];
-    }else if(month < 33 || month > 20){
-        month -= 20;
+    }else if(month < 33 && month > 20){
         day = monthDayArr[month];
-    }else if(month < 53 || month > 40){
-        month -=40;
+    }else if(month < 53 && month > 40){
         day = monthDayArr[month];
     }
     
-    if(parseInt(egn.substring(4, 6)) > day){
+    if(parseInt(egn.substring(4, 6)) >= day){
         return false;
     }
 
--- tests/isEgnGeneralValid.test.js
+++ tests/isEgnGeneralValid.test.js
@@ -6,6 +6,7 @@ test("test isEgnValidGeneral", () => {
     expect(isEgnValidGeneral("0068124568")).toBe(false)
     expect(isEgnValidGeneral("0047328800")).toBe(false)
     expect(isEgnValidGeneral("9147178800")).toBe(true)
+    expect(isEgnValidGeneral("9147318800")).toBe(true)
     expect(isEgnValidGeneral("")).toBe(false)
     expect(isEgnValidGeneral(" ")).toBe(false)
     expect(isEgnValidGeneral("6812228804")).toBe(true)
